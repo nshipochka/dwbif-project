3345ef64814de84f056d2ef1262eca3992c526b5
nshipochka <nshipochka@gmail.com>
2024-01-28 21:29:03 +0200

    Fix names, typos

--- scripts/reports/report.sql
+++ scripts/reports/report.sql
@@ -2,7 +2,7 @@
 SELECT
     t.transaction_id,
     t.amount,
-    tt.trans_type,
+    tt.t_type,
     a.account_id
 FROM f_transaction t
 JOIN d_account a ON t.account_id = a.account_id
@@ -15,7 +15,7 @@ LIMIT 10;
 SELECT
     d.month,
     a.account_id,
-    tt.trans_type,
+    tt.t_type,
     SUM(t.amount) AS total_expense
 FROM f_transaction t
 JOIN d_account a ON t.account_id = a.account_id
@@ -23,7 +23,8 @@ JOIN d_date d ON t.date = d.date
 JOIN d_transaction_type tt ON t.trans_type = tt.t_type_id
 GROUP BY
     month,
-    a.account_id
+    a.account_id,
+    tt.t_type
 ORDER BY
     month,
     a.account_id;
@@ -43,7 +44,7 @@ SELECT
     a.account_id,
     l.loan_id,
     d.date,
-    l.payment
+    l.payments,
     l.duration
 FROM f_loan l
 JOIN d_account a ON a.account_id = l.account_id
@@ -54,13 +55,13 @@ LIMIT 1;
 -- Top 10 regions with highest loan amount
 SELECT
     d.region,
-    SUM(l.loan_amount) AS total_loan_amount,
+    SUM(l.amount) AS total_loan_amount,
     ls.l_status
 FROM f_loan l
 JOIN d_account a ON l.account_id = a.account_id
 JOIN d_demographic d ON l.account_district = d.district_id
 JOIN d_loan_status ls ON l.status = ls.l_status_id
-GROUP BY d.region
+GROUP BY d.region, l_status
 ORDER BY total_loan_amount DESC
 LIMIT 10;
 
