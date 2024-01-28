afe29ba5694d915b729624bda8ff2babbfc2fb61
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2024-01-28 22:16:00 +0200

    Add fix for two queries

--- scripts/reports/report.sql
+++ scripts/reports/report.sql
@@ -43,12 +43,10 @@ GROUP BY d.region;
 SELECT
     a.account_id,
     l.loan_id,
-    d.date,
     l.payments,
     l.duration
 FROM f_loan l
 JOIN d_account a ON a.account_id = l.account_id
-JOIN d_date d ON a.date = d.date
 ORDER BY l.duration DESC
 LIMIT 1;
 
@@ -61,7 +59,7 @@ FROM f_loan l
 JOIN d_account a ON l.account_id = a.account_id
 JOIN d_demographic d ON l.account_district = d.district_id
 JOIN d_loan_status ls ON l.status = ls.l_status_id
-GROUP BY d.region, l_status
+GROUP BY d.region
 ORDER BY total_loan_amount DESC
 LIMIT 10;
 
